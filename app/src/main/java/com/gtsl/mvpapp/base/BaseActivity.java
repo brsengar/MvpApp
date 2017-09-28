d09a303eec2416088b26d1b82c33b4c35ecb40e2
Braj Sengar <braj.sengar@gmail.com>
2017-09-28 13:10:55 +0100

    first running application

--- app/src/main/java/com/gtsl/mvpapp/base/BaseActivity.java
+++ app/src/main/java/com/gtsl/mvpapp/base/BaseActivity.java
@@ -5,7 +5,6 @@ import com.gtsl.mvpapp.di.component.ActivityComponent;
 import com.gtsl.mvpapp.di.component.DaggerActivityComponent;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,8 +12,8 @@ public class BaseActivity extends AppCompatActivity {
     private ActivityComponent mActivityComponent;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         mActivityComponent = DaggerActivityComponent.builder()
                 .applicationComponent(((MvpApplication) getApplication()).applicationComponent())
